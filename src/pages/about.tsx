40a33f61b0153d74d2ac000653e11763566ac3e1
Tran Thanh Long <19dh110248@st.huflit.edu.vn>
2023-02-20 21:55:42 +0700

    practice 02

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,8 @@
 import dynamic from 'next/dynamic'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import useSWR from 'swr'
 
 //When we don't want to render at severside, we can use dynamic
 
@@ -11,14 +12,14 @@ export interface AboutProps {}
 
 export default function About(props: AboutProps) {
   const router = useRouter()
-  const [posts, setPosts] = useState([])
-  useEffect(() => {
-    ;(async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/todos')
-      const data = await res.json()
-      setPosts(data)
-    })()
-  }, [])
+  const fetcher = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await res.json()
+    return data
+  }
+  const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/todos', fetcher)
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
 
   function handleNextPage() {
     router.push({
@@ -34,7 +35,7 @@ export default function About(props: AboutProps) {
       <Header />
       <div className="text-slate-700 text-7xl">About</div>
       <ul>
-        {posts.map((post: any) => (
+        {data.map((post: any) => (
           <li key={post.id}>
             <Link href={`/posts/${post.id}`}>{post.title}</Link>
           </li>
